d80ed6edaf7f27ef67476b50d6274636c52c6961
EnzoBernardis2007 <enzobernardis2007@gmail.com>
2024-11-29 09:29:34 -0300

    added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
+import { useAuth } from '../components/AuthProvider';
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const { login } = useAuth()
+    const navigate = useNavigate()
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(formData)
+        })
+
+        if(response.ok) {
+            navigate('/about')
+        }
+    }
+
     return (
-        <>
-            <p>Login</p>
-        </>
+        <form onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <input 
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+            />
+            <label>Senha:</label>
+            <input 
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+            />
+
+            <button type="submit">Entrar</button>
+        </form>
     )
 }
 
